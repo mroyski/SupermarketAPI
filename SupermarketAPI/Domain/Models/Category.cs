b832963b5d6e91c57e6ef77d347b983793f47a56
mroyski <mroyski@gmail.com>
2019-07-20 23:46:52 -0400

    recommit

--- SupermarketAPI/Domain/Models/Category.cs
+++ SupermarketAPI/Domain/Models/Category.cs
@@ -7,5 +7,8 @@ namespace SupermarketAPI.Domain.Models
 {
     public class Category
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Product> Products { get; set; } = new List<Product>();
     }
 }
--- SupermarketAPI/Domain/Models/EUnitOfMeasurement.cs
+++ SupermarketAPI/Domain/Models/EUnitOfMeasurement.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SupermarketAPI.Domain.Models
 {
-    public class EUnitOfMeasurement
+    public enum EUnitOfMeasurement : byte
     {
+        [Description("UN")]
+        Unity = 1,
+
+        [Description("MG")]
+        Milligram = 2,
+
+        [Description("G")]
+        Gram = 3,
+
+        [Description("KG")]
+        Kilogram = 4,
+
+        [Description("L")]
+        Liter = 5
     }
 }
