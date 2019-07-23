5bf406e8852e95d70ce362b3731bda930985cd00
mroyski <mroyski@gmail.com>
2019-07-22 21:54:50 -0400

    implementing ICategoryService

---
+++ SupermarketAPI/Domain/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using SupermarketAPI.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupermarketAPI.Domain.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> ListAsync();
+    }
+}
---
+++ SupermarketAPI/Services/CategoryService.cs
@@ -0,0 +1,25 @@
+﻿using SupermarketAPI.Domain.Models;
+using SupermarketAPI.Domain.Repositories;
+using SupermarketAPI.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupermarketAPI.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<IEnumerable<Category>> ListAsync()
+        {
+            return await _categoryRepository.ListAsync();
+        }
+    }
+}
