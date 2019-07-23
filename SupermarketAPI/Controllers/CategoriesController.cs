fb8560f899451098ede33ecabc2f33fd33496747
mroyski <mroyski@gmail.com>
2019-07-22 21:36:37 -0400

    async httpget in categories controller

--- SupermarketAPI/Controllers/CategoriesController.cs
+++ SupermarketAPI/Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SupermarketAPI.Domain.Models;
+using SupermarketAPI.Domain.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,18 @@ namespace SupermarketAPI.Controllers
     [Route("/api/[controller]")]
     public class CategoriesController : Controller
     {
-        
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            var categories = await _categoryService.ListAsync();
+            return categories;
+        }
     }
 }
---
+++ SupermarketAPI/Domain/Services/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using SupermarketAPI.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupermarketAPI.Domain.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> ListAsync();
+    }
+}
