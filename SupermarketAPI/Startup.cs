45c26da0fc63ddc1213ee146e6642d4e3e5a966e
mroyski <mroyski@gmail.com>
2019-07-24 00:49:30 -0400

    database context configured for dependency injection

--- SupermarketAPI/Startup.cs
+++ SupermarketAPI/Startup.cs
@@ -6,26 +6,39 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SupermarketAPI.Domain.Repositories;
+using SupermarketAPI.Domain.Services;
+using SupermarketAPI.Persistence.Contexts;
+using SupermarketAPI.Persistence.Repositories;
+using SupermarketAPI.Services;
 
 namespace SupermarketAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("supermarket-api-in-memory");
+            });
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ICategoryService, CategoryService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
