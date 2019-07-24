bc5afe8234d6dcb6b33da3e4e6755687a6a673d2
mroyski <mroyski@gmail.com>
2019-07-24 00:38:35 -0400

    base/cat repository

---
+++ SupermarketAPI/Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupermarketAPI.Domain.Models;
+using SupermarketAPI.Domain.Repositories;
+using SupermarketAPI.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupermarketAPI.Persistence.Repositories
+{
+    public class CategoryRepository : BaseRepository, ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Category>> ListAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+    }
+}
