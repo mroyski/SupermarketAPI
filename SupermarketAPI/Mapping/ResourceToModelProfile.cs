8f18905e7391d16f60dca86f4670694e96282214
mroyski <mroyski@gmail.com>
2019-07-28 21:09:19 -0400

    savecategoryresource mapped to category

---
+++ SupermarketAPI/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SupermarketAPI.Domain.Models;
+using SupermarketAPI.Resources;
+
+namespace SupermarketAPI.Mapping
+{
+    public class ResourceToModelProfile : Profile
+    {
+        public ResourceToModelProfile()
+        {
+            CreateMap<SaveCategoryResource, Category>();
+        }
+    }
+}
