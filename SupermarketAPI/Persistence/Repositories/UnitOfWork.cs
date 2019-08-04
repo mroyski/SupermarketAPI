a739bbf91753fd32dfb9fca4d5fcf21992a7d210
mroyski <mroyski@gmail.com>
2019-08-04 14:48:49 -0400

    asynchronously complete data management operations

---
+++ SupermarketAPI/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using SupermarketAPI.Domain.Repositories;
+using SupermarketAPI.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupermarketAPI.Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CompleteAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
